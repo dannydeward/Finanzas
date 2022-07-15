34f198ec577c0df37166bc81948cd1798760bb91
Danny Ramirez <58273239+dannydeward@users.noreply.github.com>
2022-07-15 18:40:40 -0300

    Update models.py

--- Finanzas_Personales/blog/models.py
+++ Finanzas_Personales/blog/models.py
@@ -1,5 +1,4 @@
 from email.headerregistry import ContentDispositionHeader
-from tkinter import image_names
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
